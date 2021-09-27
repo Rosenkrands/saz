b3f31b87ff3ea5a2bf5e9f0e82dc76bdb21f3b12
Kasper Rosenkrands <krosen17@student.aau.dk>
2021-09-27 10:57:28 +0200

    initial commit

---
+++ R/2d-instance.R
@@ -0,0 +1,131 @@
+library(tidyverse)
+library(ggvoronoi)
+
+euclid_norm <- function(x) sqrt(sum(x^2))
+
+#' Generate 2d instance
+#' 
+#' @export
+generate_2d_instance <- function(
+  no_of_points = 50, 
+  interval = c("min" = -10, "max" = 10)
+) {
+  id <- 1:no_of_points
+  x <- runif(no_of_points, min = interval["min"], max = interval["max"])
+  y <- runif(no_of_points, min = interval["min"], max = interval["max"])
+  arrival_rate <- round(runif(no_of_points, min = 1, max = 3))
+  data <- tibble(
+    "Demand point id" = id,
+    "x" = x, 
+    "y" = y, 
+    "Arrival rate" = arrival_rate
+  )
+  results <- list("data" = data, "interval" = interval)
+  return(results)
+}
+
+solve_centroid <- function(instance, no_of_centers = 4) {
+  # place centroid randomly
+  id <- 1:no_of_centers
+  x <- runif(
+    no_of_centers, 
+    min = instance$interval["min"],
+    max = instance$interval["max"]
+  )
+  y <- runif(
+    no_of_centers, 
+    min = instance$interval["min"],
+    max = instance$interval["max"]
+  )
+  centers <- tibble("Center id" = id, "x" = x, "y" = y)
+  
+  # for each demand point find the closest center
+  no_of_rows <- nrow(centers)*nrow(instance$data)
+  center_point <- integer(no_of_rows)
+  demand_point <- integer(no_of_rows)
+  distance_to_center <- double(no_of_rows)
+  iter <- 0
+  for (i in 1:nrow(centers)) {
+    for (j in 1:nrow(instance$data)) {
+      iter <- iter + 1
+      center_point[iter] <- pull(centers[i, "Center id"])
+      demand_point[iter] <- pull(instance$data[j, "Demand point id"])
+      distance_to_center[iter] <- euclid_norm(
+        c(
+          pull(instance$data[j, "x"] - centers[i, "x"]),
+          pull(instance$data[j, "y"] - centers[i, "y"])
+        )
+      ) 
+    }
+  }
+  distances <- tibble(
+    "Center id" = center_point,
+    "Demand point id" = demand_point,
+    "Distance to center" = distance_to_center
+  )
+  assignment <- distances %>%
+    group_by(`Demand point id`) %>%
+    filter(`Distance to center` == min(`Distance to center`)) %>%
+    ungroup() %>%
+    inner_join(centers, by="Center id") %>%
+    rename(x.center = x, y.center = y)
+  objective <- instance$data %>%
+    dplyr::select(`Demand point id`, `Arrival rate`) %>%
+    inner_join(
+      select(assignment, `Center id`, `Demand point id`),
+      by = "Demand point id"
+    ) %>%
+    group_by(`Center id`) %>%
+    summarise(`Arrival rate variance` = var(`Arrival rate`))
+  return(list("assignment" = assignment, "objective" = objective))
+}
+
+plot_2d_instance <- function(instance, closest) {
+  vor_data <- closest$assignment %>% 
+    select(`Center id`, x.center, y.center) %>%
+    distinct() %>%
+    mutate(`Center id` = as.character(`Center id`))
+    
+  ggplot(
+    instance$data %>%
+      inner_join(closest$assignment, by="Demand point id") %>%
+      mutate(`Center id` = as.character(`Center id`),
+             `Arrival rate` = as.character(`Arrival rate`))
+  ) +
+    geom_voronoi(
+      data = vor_data,
+      aes(x.center, y.center, fill = `Center id`),
+      alpha = .25,
+      # geom="path",
+      outline = data.frame(
+        x = 1.1*c(-10,-10,10,10),
+        y = 1.1*c(-10,10,10,-10)
+      )
+    ) +
+    stat_voronoi(
+      data = vor_data,
+      aes(x.center, y.center),
+      geom="path",
+      outline = data.frame(
+        x = 1.1*c(-10,-10,10,10),
+        y = 1.1*c(-10,10,10,-10)
+      )
+    ) +
+    geom_point(aes(
+      x, y, 
+      # label = `Arrival rate`,
+      color = `Center id`, 
+      # shape = `Arrival rate`
+    )) +
+    theme_void() +
+    theme(legend.position = "top")
+    # scale_x_continuous(limits = instance$interval) +
+    # scale_y_continuous(limits = instance$interval)
+}
+
+# instance = generate_2d_instance(
+#   no_of_points = 100,
+#   interval = c("min" = -10, "max" = 10)
+# )
+# closest = solve_centroid(instance, no_of_centers = 8)
+# plot_2d_instance(instance, closest)
